d0840fd5e2365dff2ade740915ab7ff77b02e31e
miguel <js.mc.tic@gmail.com>
2021-05-29 05:02:53 +0100

    Fixed runtests script
    Updated version with array size per test in index file

--- Src/runtests.sh
+++ Src/runtests.sh
@@ -1,15 +1,31 @@
+#!/bin/bash
 COUNTER=1
 ARRSIZE=$1
+
 
 mkdir -p "test_files/results/size_$ARRSIZE"
 cat test_files/index.txt | while read line
 do	
 	echo "Running test battery # "$COUNTER
 
+	if [ "$COUNTER" -eq "9" ]; then 
+			echo "xd"
 
-	./energy_storms_seq $ARRSIZE $line >> "test_files/results/size_$ARRSIZE/$COUNTER".txt
+			./energy_storms_seq 16 $line >> "test_files/results/09_16".txt
 
-	./energy_storms_omp $ARRSIZE $line >> "test_files/results/size_$ARRSIZE/$COUNTER".txt
+			./energy_storms_omp 16 $line >> "test_files/results/09_16".txt
+
+			./energy_storms_seq 17 $line >> "test_files/results/09_17".txt
+
+			./energy_storms_omp 17 $line >> "test_files/results/09_17".txt
+
+
+		else
+
+			./energy_storms_seq $line >> "test_files/results/$COUNTER".txt
+
+			./energy_storms_omp $line >> "test_files/results/$COUNTER".txt
+	fi
 
 	COUNTER=$((COUNTER+1))
 
