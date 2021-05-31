773bc7d2ff540970724f95093eb8973768f69469
miguel <js.mc.tic@gmail.com>
2021-05-31 23:13:32 +0100

    Runtests.sh script fix v5 and result tests from cluster uploaded

--- Src/runtests.sh
+++ Src/runtests.sh
@@ -15,10 +15,9 @@ do
 			./energy_storms_omp 2 $line >> "test_files/results/03".txt
 
 			./energy_storms_omp 4 $line >> "test_files/results/03".txt
-		fi
-		
+			
 
-		if [ "$COUNTER" -eq "4" ]; then 
+		elif [ "$COUNTER" -eq "4" ]; then 
 
 			./energy_storms_seq $line >> "test_files/results/04".txt
 
@@ -26,9 +25,8 @@ do
 
 			./energy_storms_omp 4 $line >> "test_files/results/04".txt
 
-		fi
 
-		if [ "$COUNTER" -eq "5" ]; then 
+		elif [ "$COUNTER" -eq "5" ]; then 
 
 			./energy_storms_seq $line >> "test_files/results/05".txt
 
@@ -36,9 +34,8 @@ do
 
 			./energy_storms_omp 4 $line >> "test_files/results/05".txt
 
-		fi
 
-		if [ "$COUNTER" -eq "6" ]; then 
+		elif [ "$COUNTER" -eq "6" ]; then 
 
 			./energy_storms_seq $line >> "test_files/results/06".txt
 
@@ -46,9 +43,8 @@ do
 
 			./energy_storms_omp 4 $line >> "test_files/results/06".txt
 
-		fi
 
-		if [ "$COUNTER" -eq "9" ]; then 
+		elif [ "$COUNTER" -eq "9" ]; then 
 			
 			./energy_storms_seq 16 $line >> "test_files/results/09".txt
 			./energy_storms_seq 17 $line >> "test_files/results/09".txt
@@ -56,9 +52,6 @@ do
 			
 			for T in "${THREADS[@]}"; do
 				
-
-				./energy_storms_seq 16 $line >> "test_files/results/09".txt
-
 				./energy_storms_omp $T 16 $line >> "test_files/results/09".txt
 
 				./energy_storms_omp $T 17 $line >> "test_files/results/09".txt
@@ -66,15 +59,15 @@ do
 			done
 
 
-			else
+		else
 
-				./energy_storms_seq $line >> "test_files/results/$COUNTER".txt
+			./energy_storms_seq $line >> "test_files/results/$COUNTER".txt
 
-				for T in "${THREADS[@]}"; do
+			for T in "${THREADS[@]}"; do
 
-					./energy_storms_omp $T $line >> "test_files/results/$COUNTER".txt
+				./energy_storms_omp $T $line >> "test_files/results/$COUNTER".txt
 
-				done
+			done
 		fi
 
 	COUNTER=$((COUNTER+1))
