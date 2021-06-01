95c5fd17348ace507437ae5328d8e92b15516591
jmfrazao <jm.frazao@campus.fct.unl.pt>
2021-06-01 10:54:28 +0100

    Fixed bugs
    Fixed a few bugs on the script. It is still missing the plotting.
    Fixed:
     - fixed the file reading
     - removed spaces that were not necessary
    TODO:
     - Find a way to get the size of the array on the sequential outputs
     - Get the speedup and efficiency
     - Plot results

--- Src/test_files/results/python_script.py
+++ Src/test_files/results/python_script.py
@@ -14,31 +14,34 @@ import csv
 
 def parse(filename, endResult):
     results = open(filename, "r")
-    info = results.readlines
     
-    thread_num = ''
-    layer_size = ''
+    info = [line.rstrip() for line in results]
+    
+    thread_num = '0'
+    layer_size = '35'
     time = ''
-    
+    print(info)
     os.system("echo Parsing file {}" )
+    
     for line in info:
-        
+        print(line)
         if line.startswith('Size',0):
-            layer_size = line.split(':')[1].replace("\n", "")
+            layer_size = line.split(':')[1].replace("\n", "").replace(" ", "")
+            print("HERE")
             
         elif line.startswith('Threads',0):
-            thread_num = line.split(':')[1].replace("\n", "")
+            thread_num = line.split(':')[1].replace("\n", "").replace(" ", "")
         
         elif line.startswith('Time',0):
-            time = line.split(':')[1].replace("\n", "")
+            time = line.split(':')[1].replace("\n", "").replace(" ", "")
         
             if layer_size not in endResult.keys():
                 endResult[layer_size] = {}
         
             if thread_num not in endResult[layer_size].keys():
                 endResult[layer_size][thread_num] = {}
-        
-        endResult[layer_size][thread_num] = time
+               
+            endResult[layer_size][thread_num] = time
         
 def get_final(new_file, results):
     if (os.path.isfile(new_file) == False):
@@ -61,7 +64,10 @@ def get_final(new_file, results):
 def get_results(filename):
     file = open(filename)
     resultRead = csv.DictReader(file)
+    print(resultRead)
     
-def plot(filename):
-    
-    
+endResult ={}
+parse("1.txt", endResult)
+get_final("organizedResults.txt", endResult)
+
+
